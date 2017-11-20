f3db1c88978520a8180135dd1977204df263709e
Dimitris Dimos <dimitrisdimosiaris@gmail.com>
2017-11-20 11:19:53 +0200

    Branding changes

--- js/slipScroll.js
+++ js/slipScroll.js
@@ -1,14 +1,24 @@
-var setLogo = function() {
- 	$('.moveable-name').each(function() {
-    $(this).css('top',
-      $('.default-name').offset().top -
-      $(this).closest('section').offset().top + 80
-    );
-  });
-};
- 
-$(document).scroll(function() {
-  setLogo();
+/**
+ * Scrolling menu.
+ */
+
+$('#about').on('click', function() {
+
+    $('html, body').animate({
+        scrollTop: $("#section-about").offset().top
+    }, 1500);
 });
- 
-setLogo();
+
+$('#work').on('click', function() {
+
+    $('html, body').animate({
+        scrollTop: $("#section-work").offset().top
+    }, 1500);
+});
+
+$('#contact').on('click', function() {
+
+    $('html, body').animate({
+        scrollTop: $("#section-contact").offset().top
+    }, 2000);
+});
